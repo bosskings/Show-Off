8e85d5fee3089d664a82e3c407766f55d5e0fe66
Anwasi Ayebaemi <119889433+Ayewealth@users.noreply.github.com>
2023-05-30 12:31:59 +0100

    Fourth Commit

--- js/app.js
+++ js/app.js
@@ -1,35 +1,28 @@
-
 // ===================== Dark Theme ====================
-let theme = localStorage.getItem("theme")
-const themeBtn = document.getElementById("light")
-
+let theme = localStorage.getItem("theme");
+const themeBtn = document.getElementById("light");
 
 if (theme === "dark-theme") {
-  document.body.classList.add("dark-theme")
+  document.body.classList.add("dark-theme");
 }
 
 themeBtn.onclick = function () {
   if (theme === "dark-theme") {
     localStorage.setItem("theme", "light-theme");
-    document.body.classList.remove("dark-theme")
-    theme = "light-theme"
-
-  }
-
-  else {
+    document.body.classList.remove("dark-theme");
+    theme = "light-theme";
+  } else {
     localStorage.setItem("theme", "dark-theme");
-    document.body.classList.add("dark-theme")
-    theme = "dark-theme"
+    document.body.classList.add("dark-theme");
+    theme = "dark-theme";
   }
 };
 
-
-
 // =========================== Category Clicked =======================
-let btn = document.querySelector(".category")
+let btn = document.querySelector(".category");
 
-let items = document.querySelector('.category-items')
+let items = document.querySelector(".category-items");
 
-btn.onclick = function(){
-  items.classList.toggle('none')
-}
+btn.onclick = function () {
+  items.classList.toggle("none");
+};
