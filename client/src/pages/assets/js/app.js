5b5c65774d0290fcfb5c0c11c0921982123b282f
Boss <bosswisdomkings@gmail.com>
2023-09-04 00:37:09 +0100

    back to how it was

--- client/src/pages/assets/js/app.js
+++ client/src/pages/assets/js/app.js
@@ -1,33 +1,33 @@
-// // ===================== Dark Theme ====================
-// let theme = localStorage.getItem("theme");
-// const themeBtn = document.getElementById("light");
+// ===================== Dark Theme ====================
+let theme = localStorage.getItem("theme");
+const themeBtn = document.getElementById("light");
 
-// if (theme === "dark-theme") {
-//   document.body.classList.add("dark-theme");
-// }
+if (theme === "dark-theme") {
+  document.body.classList.add("dark-theme");
+}
 
-// themeBtn.onclick = function () {
-//   if (theme === "dark-theme") {
-//     localStorage.setItem("theme", "light-theme");
-//     document.body.classList.remove("dark-theme");
-//     theme = "light-theme";
-//   } else {
-//     localStorage.setItem("theme", "dark-theme");
-//     document.body.classList.add("dark-theme");
-//     theme = "dark-theme";
-//   }
-// };
+themeBtn.onclick = function () {
+  if (theme === "dark-theme") {
+    localStorage.setItem("theme", "light-theme");
+    document.body.classList.remove("dark-theme");
+    theme = "light-theme";
+  } else {
+    localStorage.setItem("theme", "dark-theme");
+    document.body.classList.add("dark-theme");
+    theme = "dark-theme";
+  }
+};
 
-// // Get all dropdowns from the document
-// document.querySelector(".dropdown");
+// Get all dropdowns from the document
+document.querySelector(".dropdown");
 
-// const menu = document.querySelector(".menu");
-// document.querySelectorAll(".menu li");
-// const select = document.querySelector(".select");
+const menu = document.querySelector(".menu");
+document.querySelectorAll(".menu li");
+const select = document.querySelector(".select");
 
-// select.addEventListener("click", () => {
-//   menu.classList.toggle("menu-open");
-// });
+select.addEventListener("click", () => {
+  menu.classList.toggle("menu-open");
+});
 
 
 
