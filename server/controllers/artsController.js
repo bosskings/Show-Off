da6af772c58e38f5d9e371006aad7dc0b1e0d1f4
Boss <bosswisdomkings@gmail.com>
2023-09-19 16:43:47 +0100

    almost done with arts API

--- server/controllers/artsController.js
+++ server/controllers/artsController.js
@@ -10,7 +10,7 @@ const getAllArts =  async (req, res) =>{
 
 // get a single art
 const getSingleArt = async (req, res)=>{
-    const { id } = req.params
+    const {id} = req.params
 
     if(!mongoose.Types.ObjectId.isValid(id)){
         return res.status(404).json({error:"Invalid Art request"})
@@ -37,7 +37,7 @@ const createArt = async (req, res)=>{
 
 // delete an art
 const deleteArt = async (req,res)=>{
-    const id = req.params
+    const {id} = req.params
 
     if(!mongoose.Types.ObjectId.isValid(id)){
         return res.status(400).json({error: "Invalid art request"})
@@ -69,6 +69,7 @@ const updateArt = async (req, res)=>{
         }
     
         res.status(200).json(art)
+
     } catch (err) {
         console.log(err.message)
     }
